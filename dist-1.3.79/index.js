11da57179450fcd69c9f1ac8aa811aa9856388a6
leon.hy <leon.hy@alibaba-inc.com>
2023-08-25 17:45:21 +0800

    first commit

---
+++ dist-1.3.79/index.js
@@ -0,0 +1,7 @@
+import { _ as _class_call_check } from "node_modules/@swc/helpers/esm/_class_call_check";
+import test from "./util/test";
+var X = function X() {
+    "use strict";
+    _class_call_check(this, X);
+};
+console.log(test, X);
